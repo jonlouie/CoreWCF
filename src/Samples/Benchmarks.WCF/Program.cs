13793c175350e31670b85c9dc264222a8641cb00
Jon Louie <louiejon@amazon.com>
2022-05-31 15:19:56 -0700

    Set Benchmark.WCF to run as host only

--- src/Samples/Benchmarks.WCF/Program.cs
+++ src/Samples/Benchmarks.WCF/Program.cs
@@ -1,12 +1,24 @@
 ﻿using BenchmarkDotNet.Running;
+using System;
+using System.ServiceModel;
+using Benchmarks.WCF.Services;
 
 namespace Benchmarks.WCF
 {
     internal class Program
     {
+        private static ServiceHost _host;
+
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            _host = new ServiceHost(typeof(EchoService));
+            _host.Open();
+            Console.WriteLine("Server started! Press Enter to close.");
+            Console.ReadLine();
+            _host.Close();
+
+
+            //var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
 
             // Used for debugging
             //var httpBindingBenchmarks = new HttpBindingBenchmarks();
