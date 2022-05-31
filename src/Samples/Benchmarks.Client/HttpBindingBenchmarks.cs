9a758a4f4069f9aa353db4e422a261ea9b0f4298
Jon Louie <louiejon@amazon.com>
2022-05-31 15:58:11 -0700

    add separate debug methods

--- src/Samples/Benchmarks.Client/HttpBindingBenchmarks.cs
+++ src/Samples/Benchmarks.Client/HttpBindingBenchmarks.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Collections.Generic;
 using System.ServiceModel;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
@@ -14,9 +16,9 @@ namespace Benchmarks.Client
     [SimpleJob(RunStrategy.Throughput, launchCount: 1, warmupCount: 0, targetCount: 100)]
     public class HttpBindingBenchmarks
     {
-        private const string HostName = "localhost";
-        private const string Port = "8080";
-        private readonly string _benchmarkEndpointAddress = $"http://{HostName}:{Port}/BasicWcfService/basichttp.svc";
+        //private const string HostName = "localhost";
+        //private const string Port = "8080";
+        private readonly string _benchmarkEndpointAddress = $"http://{Program.HostName}:{Program.Port}/BasicWcfService/basichttp.svc";
         private readonly IEnumerable<SampleData> _dataList1 = DataGenerator.GenerateRecords(1);
         private readonly IEnumerable<SampleData> _dataList100 = DataGenerator.GenerateRecords(100);
         private readonly IEnumerable<SampleData> _dataList1000 = DataGenerator.GenerateRecords(1000);
@@ -107,6 +109,42 @@ namespace Benchmarks.Client
         {
             // Always save the returned value or the call will be optimized away, preventing benchmark execution
             var result = _channel.SendSampleData(_dataList1000);
+        }
+    }
+    public class DebugCalls
+    {
+        private readonly string _benchmarkEndpointAddress = $"http://{Program.HostName}:{Program.Port}/BasicWcfService/basichttp.svc";
+        private readonly IEnumerable<SampleData> _dataList1 = DataGenerator.GenerateRecords(1);
+        private readonly IEnumerable<SampleData> _dataList100 = DataGenerator.GenerateRecords(100);
+        private readonly IEnumerable<SampleData> _dataList1000 = DataGenerator.GenerateRecords(1000);
+        private ClientContract.IEchoService _channel;
+
+        public void DebugSetup()
+        {
+            var httpBinding = new BasicHttpBinding
+            {
+                SendTimeout = TimeSpan.FromMinutes(20.0),
+                ReceiveTimeout = TimeSpan.FromMinutes(20.0),
+                OpenTimeout = TimeSpan.FromMinutes(20.0),
+                CloseTimeout = TimeSpan.FromMinutes(20.0),
+                MaxBufferSize = int.MaxValue,
+                MaxReceivedMessageSize = int.MaxValue
+            };
+            var endpointAddress = new EndpointAddress(new Uri(_benchmarkEndpointAddress));
+            var factory = new ChannelFactory<ClientContract.IEchoService>(httpBinding, endpointAddress);
+            _channel = factory.CreateChannel();
+            ((System.ServiceModel.Channels.IChannel)_channel).Open();
+        }
+
+        public void DebugCleanup()
+        {
+            ((System.ServiceModel.Channels.IChannel)_channel).Close();
+        }
+
+        public void DebugEchoSampleData1()
+        {
+            // Always save the returned value or the call will be optimized away, preventing benchmark execution
+            var result = _channel.EchoSampleData(_dataList1);
         }
     }
 }
--- src/Samples/Benchmarks.Client/Program.cs
+++ src/Samples/Benchmarks.Client/Program.cs
@@ -1,20 +1,25 @@
 ﻿using BenchmarkDotNet.Running;
+using System;
 
 namespace Benchmarks.Client
 {
-    internal class Program
+    public class Program
     {
+        public const string HostName = "ec2-35-86-186-181.us-west-2.compute.amazonaws.com";
+        public const string Port = "8080";
+
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            //var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
 
             // Used for debugging
-            //var httpBindingBenchmarks = new HttpBindingBenchmarks();
-            //httpBindingBenchmarks.HttpBindingGlobalSetup();
-            ////httpBindingBenchmarks.HttpBindingEchoSampleData1000();
-            ////httpBindingBenchmarks.HttpBindingReceiveSampleData1000();
-            //httpBindingBenchmarks.HttpBindingSendSampleData1000();
-            //httpBindingBenchmarks.HttpBindingGlobalCleanup();
+            var debugCalls = new DebugCalls();
+            Console.WriteLine("Setup");
+            debugCalls.DebugSetup();
+            Console.WriteLine("Making the call");
+            debugCalls.DebugEchoSampleData1();
+            Console.WriteLine("Cleaning up");
+            debugCalls.DebugCleanup();
         }
     }
 }
