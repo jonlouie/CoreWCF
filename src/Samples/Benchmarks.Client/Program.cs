6931b51a6261fdf1642475506f20b4205f87ff57
Jon Louie <louiejon@amazon.com>
2022-06-03 13:31:39 -0700

    Handle CLI params

--- src/Samples/Benchmarks.Client/Program.cs
+++ src/Samples/Benchmarks.Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Running;
 using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using Benchmarks.Common.DataContract;
 
 namespace Benchmarks.Client
 {
@@ -9,22 +10,58 @@ namespace Benchmarks.Client
         public const string HostName = "localhost";
         public const string Port = "8080";
 
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            //var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            if (args.Length == 0)
+            {
+                var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            }
+            else
+            {
+                var nonBenchmarkCalls = new NonBenchmarkCalls();
+                Console.WriteLine("Setup");
+                nonBenchmarkCalls.Setup();
 
-            // Used for debugging
-            var nonBenchmarkCalls = new NonBenchmarkCalls();
-            Console.WriteLine("Setup");
-            nonBenchmarkCalls.Setup();
+                Console.WriteLine("Making the call");
+                var command = args[0].ToLower();
+                var size = int.Parse(args[1]);
 
-            Console.WriteLine("Making the call");
-            nonBenchmarkCalls.EchoSampleDataStress(nonBenchmarkCalls.DataList100);
-            nonBenchmarkCalls.ReceiveSampleDataStress(100);
-            nonBenchmarkCalls.SendSampleDataStress(nonBenchmarkCalls.DataList100);
+                IEnumerable<SampleData> data;
+                if (size == 100)
+                {
+                    data = nonBenchmarkCalls.DataList100;
+                }
+                else if (size == 1000)
+                {
+                    data = nonBenchmarkCalls.DataList1000;
+                }
+                else
+                {
+                    data = nonBenchmarkCalls.DataList1;
+                    size = 1;
+                }
 
-            Console.WriteLine("Cleaning up");
-            nonBenchmarkCalls.Cleanup();
+                Console.WriteLine($"Command == {command}");
+                Console.WriteLine($"Data size == {size}");
+                switch (command)
+                {
+                    case "echo":
+                        nonBenchmarkCalls.EchoSampleDataStress(data);
+                        break;
+                    case "receive":
+                        nonBenchmarkCalls.ReceiveSampleDataStress(size);
+                        break;
+                    case "send":
+                        nonBenchmarkCalls.SendSampleDataStress(data);
+                        break;
+                    default:
+                        Console.WriteLine("Unrecognized arg");
+                        break;
+                }
+
+                Console.WriteLine("Cleaning up");
+                nonBenchmarkCalls.Cleanup();
+            }
         }
     }
 }
